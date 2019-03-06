691cccc147d64b53d7342224a6e619358162897b
Abelhg <abelherrero98@gmail.com>
2019-03-06 17:22:57 +0100

    Preparada Practica 004

---
+++ 004-CasaDomotica/src/casadomotica/Main.java
@@ -0,0 +1,17 @@
+package casadomotica;
+
+import casadomotica.vista.PanelVista;
+
+/**
+ * Se encarga de ejecutar la aplicación.
+ * 
+ * @author abeherr
+ * @author hechila
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new PanelVista().setVisible(true);
+    }
+    
+}
